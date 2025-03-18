e97d7ee098d9342e948ca5f9469db525feaf9544
alifsyr <alifsyr@gmail.com>
2025-03-18 19:20:58 +0700

    Add logo to login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -3,7 +3,15 @@ import Form from "../components/Form";
 function LoginPage() {
     return (
         <div>
-            <h1>Login Page</h1>
+            <div className="logo-container">
+                <img 
+                    src="https://object-dataku.ap-south-1.linodeobjects.com/uploads/all/linodex1669942542_logo.jpg" 
+                    alt="Toko Madure Logo" 
+                    className="logo" 
+                />
+            </div>
+            <h1>Toko Madure</h1>
+            <h2>Login</h2>
             <Form />
         </div>
     )
